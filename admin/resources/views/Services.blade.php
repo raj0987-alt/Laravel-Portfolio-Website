c03b34e671befeae4a3737f2cade3a0139f298c7
Rashadul Alam <71288207+raj0987-alt@users.noreply.github.com>
2021-09-23 17:18:46 +0600

    loader svg add

--- admin/resources/views/Services.blade.php
+++ admin/resources/views/Services.blade.php
@@ -1,7 +1,7 @@
 @extends('Layout.app')
 
 @section('content')
-<div class="container">
+<div class="container"> <!-- load services using javascript start -->
 <div class="row">
 <div class="col-md-12 p-5">
 <table id="" class="table table-striped table-bordered" cellspacing="0" width="100%">
@@ -25,7 +25,19 @@
 
 </div>
 </div>
+</div> <!-- load services using javascript end -->
+
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-center p-5">
+	<img class="w-25" src="{{asset('images/loader.svg')}}">
+
+
 </div>
+</div>
+</div>
+
+
 
 @endsection
 
